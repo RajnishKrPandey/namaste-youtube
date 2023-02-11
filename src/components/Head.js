aca95d6e0165b8ab2147a6ed41ced7cdcd7620d0
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-11 21:46:45 +0530

    Create Header Component

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Head = () => {
   return (
-    <div>Head</div>
-  )
-}
+    <div>
+      <div>
+        <img
+          alt="menu"
+          src="https://tse3.mm.bing.net/th?id=OIP.NbPVwuvEiIQI4Ry2dWX9EQAAAA&pid=Api&P=0"
+        />
+        <img
+          alt="youtube-logo"
+          src="https://www.pngfind.com/pngs/m/298-2981353_youtube-logo-hd-png-download.png"
+        />
+      </div>
+      <div>
+        <input type="text" />
+        <button>Search</button>
+      </div>
+      <div>
+        <img
+          alt="user"
+          src="https://tse3.mm.bing.net/th?id=OIP.HHVUf3TYqncgpJXyCMmxyAHaHa&pid=Api&P=0"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Head
+export default Head;
