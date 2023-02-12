1825695ebc2eddd23ca0aeef7bb6adc24ab802eb
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-12 08:39:29 +0530

    Style Header component

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,25 +2,40 @@ import React from 'react';
 
 const Head = () => {
   return (
-    <div>
-      <div>
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1 ">
         <img
+          className="h-8"
           alt="menu"
           src="https://tse3.mm.bing.net/th?id=OIP.NbPVwuvEiIQI4Ry2dWX9EQAAAA&pid=Api&P=0"
         />
         <img
+          className="h-8 mx-2"
           alt="youtube-logo"
-          src="https://www.pngfind.com/pngs/m/298-2981353_youtube-logo-hd-png-download.png"
+          src="https://tse4.mm.bing.net/th?id=OIP.gdsYnuzqxcjHkrkFHhMYFAHaBw&pid=Api&P=0"
         />
       </div>
-      <div>
-        <input type="text" />
-        <button>Search</button>
+      <div className=" col-span-10 px-10">
+        <input
+          className=" w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className=" border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+        <button>
+          <img
+            className="h-8 inline-block px-5"
+            alt="voice-icon"
+            src="https://tse1.mm.bing.net/th?id=OIP.2PbiXNCGCA7sKYyEr6QbCQHaHa&pid=Api&P=0"
+          />
+        </button>
       </div>
-      <div>
+      <div className=" col-span-1">
         <img
+          className="h-8"
           alt="user"
-          src="https://tse3.mm.bing.net/th?id=OIP.HHVUf3TYqncgpJXyCMmxyAHaHa&pid=Api&P=0"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
         />
       </div>
     </div>
